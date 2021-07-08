cc62a20d448848f6586366e121cdef035678b1ad
silambarasan <silambarasan.karthikeyan@gmail.com>
2021-07-08 13:17:44 +0530

    adding jenkinsfile

---
+++ vagrant-ubuntu/ubuntu/vagrant/gitlab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install gitlab
+
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
--- vagrant-ubuntu/ubuntu/vagrant/jenkins.sh
+++ vagrant-ubuntu/ubuntu/vagrant/jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt-get update 
 
 # Install Python3
 
@@ -11,18 +11,19 @@ sudo apt-get -y install python3 \
                         
 
 # Install java and jenkins
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
-    /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
+    /etc/apt/sources.list.d/jenkins.list' && \
+sudo apt-get update && \
 sudo apt -y install openjdk-8-jdk && \
 sudo apt-get -y install jenkins && \
+sudo systemctl enable jenkins.service && \ 
 sudo systemctl start jenkins.service
-sudo systemctl enable jenkins.service
+
 
 
 # Install Ansible
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get update
-sudo apt -y install ansible
+sudo add-apt-repository --yes --update ppa:ansible/ansible && \
+sudo apt-get update && \
+sudo apt -y install ansible 
 
